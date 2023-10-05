2335408bfffc87c25a3871897109f5fbee92dad4
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-05 18:38:37 +0530

    Added a function get the IP address of the device that can connect to the internet

--- utils/utils.go
+++ utils/utils.go
@@ -1,2 +1,20 @@
 // Package utils contains utility functions
 package utils
+
+import (
+	"net"
+)
+
+// GetOutBoundIP is a function that is used to get the IP address of the device that can
+// connect to the internet
+func GetOutBoundIP() (ip net.IP, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP, nil
+}
