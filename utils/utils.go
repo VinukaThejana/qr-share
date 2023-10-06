d5e94ec705c5b0805ad9266354acd28f45edb69e
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-06 12:15:57 +0530

    Created a function to extract the filename and the dir path from the filepath string

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,72 @@
 package utils
 
 import (
+	"fmt"
 	"net"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
+
+// GetDirAndFile is a function that is used to get the directory
+// and the file from the given file path
+func GetDirAndFile(filePath string) (dir string, filename string, err error) {
+	if !filepath.IsAbs(filePath) {
+		filePath, err = filepath.Abs(filePath)
+		if err != nil {
+			return "", "", err
+		}
+	}
+
+	var seperator string
+	if runtime.GOOS == "windows" {
+		seperator = `\`
+	} else {
+		seperator = "/"
+	}
+
+	path := filePath
+	if runtime.GOOS != "windows" && strings.Contains(filePath, "~/") {
+		currentUser, err := user.Current()
+		data := strings.Split(filePath, "~/")
+
+		if err != nil {
+			return "", "", err
+		}
+		if runtime.GOOS == "darwin" {
+			path = fmt.Sprintf("/Users/%s/%s", currentUser.Username, data[1])
+		} else {
+			path = fmt.Sprintf("/home/%s/%s", currentUser.Username, data[1])
+		}
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return "", "", err
+	}
+
+	if info.IsDir() {
+		return path, "", nil
+	}
+
+	data := strings.Split(path, seperator)
+	if len(data) == 0 {
+		return "", "", fmt.Errorf("unexpected error")
+	}
+	filename = url.QueryEscape(data[len(data)-1])
+
+	for i, value := range data {
+		if i == len(data)-1 || i == 0 {
+			continue
+		}
+
+		dir = dir + seperator + value
+	}
+	return dir, filename, nil
+}
 
 // GetOutBoundIP is a function that is used to get the IP address of the device that can
 // connect to the internet
